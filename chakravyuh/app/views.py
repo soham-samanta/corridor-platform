0603101869f186f0518653bb3fe446051cdd0fa8
Soham Samanta <72293985+soham-samanta@users.noreply.github.com>
2023-03-25 12:38:29 +0530

    x , y , threshold logic done

--- chakravyuh/app/views.py
+++ chakravyuh/app/views.py
@@ -13,12 +13,20 @@ def index1(req):
 def handle_file_upload(request):
     if request.method == 'POST' and 'csv-file' in request.FILES:
         csv_file = request.FILES['csv-file']
+        x_value = request.POST['x-value']
+        y_value = request.POST['y-value']
+        t_value = request.POST['t-value']
         data_dir = os.path.join(settings.BASE_DIR, 'data')
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
         with open(os.path.join(data_dir, csv_file.name), 'wb') as f:
             for chunk in csv_file.chunks():
                 f.write(chunk)
+        with open(os.path.join(data_dir, 'values.txt'), 'w') as f:
+            f.write(x_value + '\n')
+            f.write(y_value + '\n')
+            f.write(t_value + '\n')
+            
         return render(request, 'index.html')
     else:
         return render(request, 'index.html')
